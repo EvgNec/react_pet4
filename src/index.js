5a18767455e6a7f50c294a05479f1373e7e9265e
Evgeniy <evgnec@gmail.com>
2025-06-28 17:26:48 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import AuthProvider from './context/auth/Provider';
@@ -7,7 +8,7 @@ import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>

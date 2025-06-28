9a15793530e83facdb2a6d46c08215cb72d6e49d
Evgeniy <evgnec@gmail.com>
2025-06-28 19:37:11 +0300

    upd

--- src/components/PhoneBook/Filter/Filter.jsx
+++ src/components/PhoneBook/Filter/Filter.jsx
@@ -3,13 +3,13 @@ import css from './Filter.module.css'
 export default function Filter({value, onCharge}) {
     return (
           <label className={css.Filter__label}>
-            Name
+            Filter
             <input
               className={css.Filter__name}
               type="text"
               name="filter"
               required
-              placeholder="filter name"
+              placeholder="search name"
               onChange={onCharge}
               value={value}
             />
